4281973471fe9547274a7b36a508b45fdb11c43d
justgo97 <hamdi2050@live.com>
2017-05-03 12:36:52 +0200

    clean up

--- regamedll/dlls/game.h
+++ regamedll/dlls/game.h
@@ -144,15 +144,7 @@ extern cvar_t freeforall;
 extern cvar_t auto_join_team;
 extern cvar_t max_teamkills;
 extern cvar_t fraglimit;
-<<<<<<< HEAD
-<<<<<<< HEAD
 extern cvar_t chat_flags;
-=======
-
->>>>>>> parent of d9ea9bd... added chat flags
-=======
-
->>>>>>> parent of d9ea9bd... added chat flags
 extern cvar_t showtriggers;
 extern cvar_t hostagehurtable;
 extern cvar_t roundover;
