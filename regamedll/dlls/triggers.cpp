7a5cd6dba95efe5621eda17b3595b3c313f17ed9
Dmitry Novikov <s1lentsk@yandex.ru>
2017-05-07 01:41:55 +0700

    Update triggers.cpp
    fix bracket

--- regamedll/dlls/triggers.cpp
+++ regamedll/dlls/triggers.cpp
@@ -1728,7 +1728,7 @@ void CBuyZone::__MAKE_VHOOK(Spawn)()
 void CBuyZone::BuyTouch(CBaseEntity *pOther)
 {
 #ifdef REGAMEDLL_ADD
-	if( buytime.value == 0.0f)
+	if (buytime.value == 0.0f)
 		return;
 #endif	
 	
