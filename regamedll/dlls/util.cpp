1722f883a12363e59b3cb1a585c1699f3d3b5624
Alexander B <sania40000@yandex.ru>
2019-04-08 12:39:48 +0300

    Buffer overflow fix (#351)
    * Buffer overflow fix

--- regamedll/dlls/util.cpp
+++ regamedll/dlls/util.cpp
@@ -1248,7 +1248,7 @@ void UTIL_StringToVector(float *pVector, const char *pString)
 	char tempString[128];
 	int j;
 
-	Q_strcpy(tempString, pString);
+	Q_strlcpy(tempString, pString);
 
 	pstr = tempString;
 	pfront = tempString;
@@ -1315,7 +1315,7 @@ void UTIL_StringToIntArray(int *pVector, int count, const char *pString)
 	char tempString[128];
 	int j;
 
-	Q_strcpy(tempString, pString);
+	Q_strlcpy(tempString, pString);
 
 	pstr = tempString;
 	pfront = tempString;
