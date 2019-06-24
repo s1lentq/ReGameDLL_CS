0edc285685adf76ad571d3ac4be249bbd753f7b9
Vaqtincha <xasankattabekov@gmail.com>
2019-06-24 09:26:15 +0500

    fix

--- regamedll/dlls/ggrenade.cpp
+++ regamedll/dlls/ggrenade.cpp
@@ -1143,7 +1143,7 @@ void CGrenade::Use(CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useTy
 	}
 
 #ifdef REGAMEDLL_FIXES
-	if((pPlayer->pev->flags & FL_ONGROUND) != FL_ONGROUND) // Defuse should start only on ground
+	if (!m_bStartDefuse && (pPlayer->pev->flags & FL_ONGROUND) != FL_ONGROUND) // Defuse should start only on ground
 	{
 		ClientPrint(pPlayer->pev, HUD_PRINTCENTER, "#C4_Defuse_Must_Be_On_Ground");
 		return;
