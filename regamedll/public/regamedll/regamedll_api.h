197dc91d5be10b6952b884343be10fa550a7492b
fant1kua <fant1kua@users.noreply.github.com>
2019-09-19 22:38:42 +0300

    Bump version

--- regamedll/public/regamedll/regamedll_api.h
+++ regamedll/public/regamedll/regamedll_api.h
@@ -38,7 +38,7 @@
 #include <API/CSInterfaces.h>
 
 #define REGAMEDLL_API_VERSION_MAJOR 5
-#define REGAMEDLL_API_VERSION_MINOR 11
+#define REGAMEDLL_API_VERSION_MINOR 12
 
 // CBasePlayer::Spawn hook
 typedef IHookChainClass<void, class CBasePlayer> IReGameHook_CBasePlayer_Spawn;
