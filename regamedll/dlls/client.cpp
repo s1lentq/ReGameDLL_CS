1173a9ecda1ded39718d64e8852030786baf5713
Dmitry Novikov <s1lentsk@yandex.ru>
2019-09-22 21:41:41 +0700

    Update client.cpp
    Use FStrnEq instead Q_strstr

--- regamedll/dlls/client.cpp
+++ regamedll/dlls/client.cpp
@@ -3457,10 +3457,9 @@ void EXT_FUNC InternalCommand(edict_t *pEntity, const char *pcmd, const char *pa
 #ifdef REGAMEDLL_ADD
 			else if (FStrEq(pcmd, "give"))
 			{
-				if (CVAR_GET_FLOAT("sv_cheats") != 0.0f && CMD_ARGC() > 1 && Q_strstr(parg1, "weapon_"))
+				if (CVAR_GET_FLOAT("sv_cheats") != 0.0f && CMD_ARGC() > 1 && FStrnEq(parg1, "weapon_", sizeof("weapon_") - 1))
 				{
 					const auto pInfo = GetWeaponInfo(parg1);
-
 					if (pInfo)
 					{
 						if (pInfo->id != WEAPON_GLOCK && pInfo->id != WEAPON_C4 /* && pInfo->id != WEAPON_KNIFE */)
