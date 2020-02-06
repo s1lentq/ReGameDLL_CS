aea65d5cafb16e18faa43b54d0282c7ffe77f391
A. Shkarlatov <crazyalex25@mail.ru>
2020-02-06 23:02:16 +0400

    Update cs_bot_manager.cpp
    Fix bug: if bot_autokill_delay > round time  m_flNextAutoKillCheck is no update. And in next round bot not will be auto killed

--- regamedll/dlls/bot/cs_bot_manager.cpp
+++ regamedll/dlls/bot/cs_bot_manager.cpp
@@ -1056,7 +1056,11 @@ void CCSBotManager::MonitorBotAutoKill()
 		return;
 
 	if (m_isRoundOver)
+	{
+		m_flNextAutoKillCheck = 0.0f;
 		return;
+	}
+		
 
 	for (int iIndex = 1; iIndex <= gpGlobals->maxClients; iIndex++)
 	{
@@ -1091,7 +1095,6 @@ void CCSBotManager::MonitorBotAutoKill()
 	if (gpGlobals->time >= m_flNextAutoKillCheck)
 	{
 		SERVER_COMMAND("bot_kill\n");
-		m_flNextAutoKillCheck = 0.0f;
 	}
 
 
