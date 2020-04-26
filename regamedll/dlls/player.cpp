47299673d51db53ae2e26c88e963dd8c5a3dc207
Vaqtincha <xasankattabekov@gmail.com>
2020-04-26 23:22:42 +0500

    remove space

--- regamedll/dlls/player.cpp
+++ regamedll/dlls/player.cpp
@@ -834,7 +834,7 @@ BOOL EXT_FUNC CBasePlayer::__API_HOOK(TakeDamage)(entvars_t *pevInflictor, entva
 		CBaseEntity *pAttacker = GET_PRIVATE<CBaseEntity>(ENT(pevAttacker));
 
 		// don't take damage if victim has protection
-		if (((pAttacker && pAttacker->IsPlayer()) || (bitsDamageType & DMG_FALL))  && CSPlayer()->GetProtectionState() == CCSPlayer::ProtectionSt_Active)
+		if (((pAttacker && pAttacker->IsPlayer()) || (bitsDamageType & DMG_FALL)) && CSPlayer()->GetProtectionState() == CCSPlayer::ProtectionSt_Active)
 			return FALSE;
 	}
 #endif
