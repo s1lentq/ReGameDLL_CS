6c80744146fb583d066be0a2977225da29e575fe
Vaqtincha <51029683+Vaqtincha@users.noreply.github.com>
2020-05-27 06:26:45 +0500

    Add new CVar mp_falldamage (#539)
    * Add CVar mp_falldamage

--- regamedll/dlls/game.cpp
+++ regamedll/dlls/game.cpp
@@ -147,6 +147,8 @@ cvar_t ff_damage_reduction_other        = { "ff_damage_reduction_other",        
 
 cvar_t radio_timeout           = { "mp_radio_timeout", "1.5", FCVAR_SERVER, 1.5f, nullptr };
 cvar_t radio_maxinround        = { "mp_radio_maxinround", "60", FCVAR_SERVER, 60.0f, nullptr };
+cvar_t falldamage              = { "mp_falldamage", "1", FCVAR_SERVER, 1.0f, nullptr };
+
 
 void GameDLL_Version_f()
 {
@@ -359,7 +361,8 @@ void EXT_FUNC GameDLLInit()
 
 	CVAR_REGISTER(&radio_timeout);
 	CVAR_REGISTER(&radio_maxinround);
-
+	CVAR_REGISTER(&falldamage);
+	
 	// print version
 	CONSOLE_ECHO("ReGameDLL version: " APP_VERSION "\n");
 
