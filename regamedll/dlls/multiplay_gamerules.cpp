39ebdebee4622f0c333714644c05e296ed2e3bb8
Vaqtincha <xasankattabekov@gmail.com>
2021-01-12 11:22:24 +0500

    enhance CVar "mp_roundover"

--- regamedll/dlls/multiplay_gamerules.cpp
+++ regamedll/dlls/multiplay_gamerules.cpp
@@ -3017,9 +3017,18 @@ void CHalfLifeMultiplay::CheckRoundTimeExpired()
 #ifdef REGAMEDLL_ADD
 	else if (roundover.value)
 	{
-		// round is over
-		if (!OnRoundEnd_Intercept(WINSTATUS_DRAW, ROUND_GAME_OVER, GetRoundRestartDelay()))
-			return;
+		switch ((int)roundover.value)
+		{
+		case 1:
+			if (!OnRoundEnd_Intercept(WINSTATUS_TERRORISTS, ROUND_TERRORISTS_WIN, CSGameRules()->GetRoundRestartDelay()))
+				return;
+		case 2:
+			if(!OnRoundEnd_Intercept(WINSTATUS_CTS, ROUND_CTS_WIN, CSGameRules()->GetRoundRestartDelay()))
+				return;
+		default:
+			if (!OnRoundEnd_Intercept(WINSTATUS_DRAW, ROUND_GAME_OVER, GetRoundRestartDelay()))
+				return;
+		}	
 	}
 #endif
 
