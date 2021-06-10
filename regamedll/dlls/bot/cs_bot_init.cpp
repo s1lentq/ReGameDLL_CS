404cd78e21a7270460ffdf6cea8cd9d553ac7ac8
Vaqtincha <xasankattabekov@gmail.com>
2021-06-10 23:08:51 +0500

    "bot_agressive" set default to 0

--- regamedll/dlls/bot/cs_bot_init.cpp
+++ regamedll/dlls/bot/cs_bot_init.cpp
@@ -62,7 +62,7 @@ cvar_t cv_bot_deathmatch             = { "bot_deathmatch", "0", FCVAR_SERVER, 0.
 cvar_t cv_bot_quota_mode             = { "bot_quota_mode", "normal", FCVAR_SERVER, 0.0f, nullptr };
 cvar_t cv_bot_join_delay             = { "bot_join_delay", "0", FCVAR_SERVER, 0.0f, nullptr };
 cvar_t cv_bot_freeze                 = { "bot_freeze", "0", 0, 0.0f, nullptr };
-cvar_t cv_bot_agressive              = { "bot_agressive", "1", FCVAR_SERVER, 0.0f, nullptr };
+cvar_t cv_bot_agressive              = { "bot_agressive", "0", FCVAR_SERVER, 0.0f, nullptr };
 #else
 // Migrated to bot_quota_mode, use "match"
 cvar_t cv_bot_quota_match            = { "bot_quota_match", "0", FCVAR_SERVER, 0.0f, nullptr };
