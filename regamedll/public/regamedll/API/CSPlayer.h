a2a3501936512ddcb692e0a47d995fc3a0d3eced
Alejo <alejoperezherrero@gmail.com>
2021-10-27 22:06:06 -0300

    API member for CVar `m_bCanPlantAnywhere`

--- regamedll/public/regamedll/API/CSPlayer.h
+++ regamedll/public/regamedll/API/CSPlayer.h
@@ -48,7 +48,8 @@ public:
 		m_bCanShootOverride(false),
 		m_bGameForcingRespawn(false),
 		m_bAutoBunnyHopping(false),
-		m_bMegaBunnyJumping(false)
+		m_bMegaBunnyJumping(false),
+		m_bCanPlantAnywhere(false)
 	{
 		m_szModel[0] = '\0';
 	}
@@ -129,6 +130,7 @@ public:
 	bool m_bGameForcingRespawn;
 	bool m_bAutoBunnyHopping;
 	bool m_bMegaBunnyJumping;
+	bool m_bCanPlantAnywhere;
 };
 
 // Inlines
