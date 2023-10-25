426c975a885164ebd90ccf2a92183c89a94e291e
Vaqtincha <51029683+Vaqtincha@users.noreply.github.com>
2023-10-25 09:37:32 +0500

    Ammo type hardcode fix (#882)
    * Useful for custom ammo types.

--- regamedll/dlls/client.cpp
+++ regamedll/dlls/client.cpp
@@ -2251,7 +2251,7 @@ bool EXT_FUNC __API_HOOK(BuyGunAmmo)(CBasePlayer *pPlayer, CBasePlayerItem *weap
 	if (pPlayer->m_iAccount >= info->clipCost)
 	{
 #ifdef REGAMEDLL_FIXES
-		if (pPlayer->GiveAmmo(info->buyClipSize, info->ammoName2, weapon->iMaxAmmo1()) == -1)
+		if (pPlayer->GiveAmmo(info->buyClipSize, weapon->pszAmmo1(), weapon->iMaxAmmo1()) == -1)
 			return false;
 
 		EMIT_SOUND(ENT(weapon->pev), CHAN_ITEM, "items/9mmclip1.wav", VOL_NORM, ATTN_NORM);
@@ -4813,7 +4813,7 @@ int EXT_FUNC GetWeaponData(edict_t *pEdict, struct weapon_data_s *info)
 						const WeaponInfoStruct *wpnInfo = GetDefaultWeaponInfo(II.iId);
 
 						if (wpnInfo && wpnInfo->gunClipSize != II.iMaxClip)
-							item->m_iClip = wpnInfo->gunClipSize; 
+							item->m_iClip = wpnInfo->gunClipSize;
 					}
 #endif
 				}
