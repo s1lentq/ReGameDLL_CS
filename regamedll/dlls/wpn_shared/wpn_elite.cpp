c48be874743b2a440728889acb4797f4ec04137a
Francisco Muñoz <fjmunozpena@gmail.com>
2024-08-05 21:11:59 -0400

    Extend KickBack functionalities (#980)

--- regamedll/dlls/wpn_shared/wpn_elite.cpp
+++ regamedll/dlls/wpn_shared/wpn_elite.cpp
@@ -200,7 +200,11 @@ void CELITE::ELITEFire(float flSpread, float flCycleTime, BOOL fUseSemi)
 	}
 
 	m_flTimeWeaponIdle = UTIL_WeaponTimeBase() + 2.0f;
+#ifdef REGAMEDLL_ADD
+	KickBack(2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0);
+#else
 	m_pPlayer->pev->punchangle.x -= 2.0f;
+#endif
 }
 
 void CELITE::Reload()
