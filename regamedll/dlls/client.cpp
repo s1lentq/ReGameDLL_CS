8e688d1d3a712d28215373c167564362c4fb83a6
dystopm <fjmunozpena@gmail.com>
2024-08-09 01:06:08 -0400

    Added mp_vote_flags cvar

--- regamedll/dlls/client.cpp
+++ regamedll/dlls/client.cpp
@@ -2619,6 +2619,14 @@ void EXT_FUNC InternalCommand(edict_t *pEntity, const char *pcmd, const char *pa
 	{
 		if (gpGlobals->time >= pPlayer->m_flLastCommandTime[CMD_VOTE])
 		{
+#ifdef REGAMEDLL_ADD
+			static const int flagKick = UTIL_ReadFlags("k");
+			if ((flagKick & UTIL_ReadFlags(vote_flags.string)) == 0)
+			{
+				ClientPrint(pPlayer->pev, HUD_PRINTCENTER, "#Command_Not_Available");
+				return;
+			}
+#endif
 			pPlayer->m_flLastCommandTime[CMD_VOTE] = gpGlobals->time + 0.3f;
 
 			if (gpGlobals->time < pPlayer->m_flNextVoteTime)
@@ -2700,6 +2708,14 @@ void EXT_FUNC InternalCommand(edict_t *pEntity, const char *pcmd, const char *pa
 	{
 		if (gpGlobals->time >= pPlayer->m_flLastCommandTime[CMD_VOTEMAP])
 		{
+#ifdef REGAMEDLL_ADD
+			static const int flagMap = UTIL_ReadFlags("m");
+			if ((flagMap & UTIL_ReadFlags(vote_flags.string)) == 0)
+			{
+				ClientPrint(pPlayer->pev, HUD_PRINTCENTER, "#Command_Not_Available");
+				return;
+			}
+#endif
 			pPlayer->m_flLastCommandTime[CMD_VOTEMAP] = gpGlobals->time + 0.3f;
 
 			if (gpGlobals->time < pPlayer->m_flNextVoteTime)
--- regamedll/dlls/game.cpp
+++ regamedll/dlls/game.cpp
@@ -185,6 +185,8 @@ cvar_t item_respawn_time       = { "mp_item_respawn_time", "30", FCVAR_SERVER, 3
 cvar_t weapon_respawn_time     = { "mp_weapon_respawn_time", "20", FCVAR_SERVER, 20.0f, nullptr };
 cvar_t ammo_respawn_time       = { "mp_ammo_respawn_time", "20", FCVAR_SERVER, 20.0f, nullptr };
 
+cvar_t vote_flags              = { "mp_vote_flags", "km", 0, 0.0f, nullptr };
+
 void GameDLL_Version_f()
 {
 	if (Q_stricmp(CMD_ARGV(1), "version") != 0)
@@ -453,6 +455,8 @@ void EXT_FUNC GameDLLInit()
 	CVAR_REGISTER(&item_respawn_time);
 	CVAR_REGISTER(&weapon_respawn_time);
 	CVAR_REGISTER(&ammo_respawn_time);
+
+	CVAR_REGISTER(&vote_flags);
 
 	// print version
 	CONSOLE_ECHO("ReGameDLL version: " APP_VERSION "\n");
